42b4762ee0fe65ddb906705cc5c85a76d8406d2b
Nikita Shagalov <shagalovprog@gmail.com>
2024-03-19 22:11:37 +0300

    PB-71 Добавил формирования json'а для отправки в кафку согласно модели данных бэка. Добавил mypy

--- src/example.py
+++ src/example.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 
-from AsyncKafkaHandler import get_kafka_async_logger
+from AsyncKafkaHandler import get_kafka_async_logger  # type: ignore
 
 # Your kafka server
 bootstrap_servers = 'localhost:9093'
@@ -12,6 +12,7 @@ topic_name = 'my-topic'
 async def main():
     # Setup logger
     logger = get_kafka_async_logger(
+        app_name="example app",
         name="Kafka logger",
         bootstrap_servers=bootstrap_servers,
         topic_name=topic_name,
@@ -20,7 +21,14 @@ async def main():
 
     # Write logs
     logger.info('Init kafka logger1')
-    logger.info({"someData": "Init kafka logger1"})
+    # Log in kafka topic will be:
+    # {
+    #   'app_name': 'example app',
+    #   'module': 'example',
+    #   'level': 'INFO',
+    #   'timestamp': '2024-01-01 00:00:00.000000',
+    #   'message': 'Init kafka logger1'
+    # }
 
     # Waiting async log sending
     await asyncio.sleep(1)
